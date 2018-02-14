c5a04dc0f0f92e129281446c3dbe90a4e8f1a9fc
davidcrc <david_drcr@hotmail.com>
2018-02-14 17:42:34 -0500

    Layout with Flexbox, usando diferentes vistas y propiedades, anterior vr

--- App.js
+++ App.js
@@ -1,25 +1,25 @@
 import React, { Component } from 'react';
-import { AppRegistry, View ,Text} from 'react-native';
+import { AppRegistry, View } from 'react-native';
 
-export default class FlexDimensionsBasics extends Component {
+export default class AlignItemsBasics extends Component {
   render() {
     return (
-      // Try removing the `flex: 1` on the parent View.
-      // The parent will not have dimensions, so the children can't expand.
-      // What if you add `height: 300` instead of `flex: 1`?
-      <View style={{flex: 1}}>
-        <View style={{flex: 1, backgroundColor: 'powderblue'}} >
-            <Text> Esta es la vista 1 </Text> 
-        </View>
-        <View style={{flex: 2, backgroundColor: 'skyblue', alignItems: 'center'}}>
-            <Text> Esta es la vista 2 </Text> 
-        
-        </View>
-        <View style={{flex: 3, backgroundColor: 'steelblue'}}>
-            <Text> Esta es la vista 3 de React </Text>
-        </View>
-        
+      // Try setting `alignItems` to 'flex-start'
+      // Try setting `justifyContent` to `flex-end`.
+      // Try setting `flexDirection` to `row`.
+      <View style={{
+        flex: 1,
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}>
+        <View style={{width: 50, height: 50, backgroundColor: 'powderblue'}} />
+        <View style={{width: 50, height: 50, backgroundColor: 'skyblue'}} />
+        <View style={{width: 50, height: 50, backgroundColor: 'steelblue'}} />
       </View>
     );
   }
-}
+};
+
+// skip this line if using Create React Native App
+AppRegistry.registerComponent('AwesomeProject', () => AlignItemsBasics);
