e51bc5d6e15ddc651ebd29bbb1485f613d697b35
davidcrc <david_drcr@hotmail.com>
2018-02-16 10:32:03 -0500

    Usando FlatList

--- App.js
+++ App.js
@@ -1,22 +1,36 @@
 import React, { Component } from 'react';
-import { AppRegistry, ScrollView, Image, Text } from 'react-native';
+import { AppRegistry, FlatList, StyleSheet, Text, View } from 'react-native';
 
-export default class IScrolledDownAndWhatHappenedNextShockedMe extends Component {
+export default class FlatListBasics extends Component {
   render() {
-      return (
-        <ScrollView>
-          <Text style={{fontSize:96}}>Scroll me plz</Text>
-
-          <Text style={{fontSize:96}}>If you like</Text>
-
-          <Text style={{fontSize:96}}>Scrolling down</Text>
-
-          <Text style={{fontSize:96}}>What's the best</Text>
-
-          <Text style={{fontSize:96}}>Framework around?</Text>
-
-          <Text style={{fontSize:80}}>React Native</Text>
-        </ScrollView>
+    return (
+      <View style={styles.container}>
+        <FlatList
+          data={[
+            {key: 'Devin'},
+            {key: 'Jackson'},
+            {key: 'James'},
+            {key: 'Joel'},
+            {key: 'John'},
+            {key: 'Jillian'},
+            {key: 'Jimmy'},
+            {key: 'Julie'},
+          ]}
+          renderItem={({item}) => <Text style={styles.item}>{item.key}</Text>}
+        />
+      </View>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+   flex: 1,
+   paddingTop: 22
+  },
+  item: {
+    padding: 10,
+    fontSize: 18,
+    height: 44,
+  },
+})
