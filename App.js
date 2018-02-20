407167ee7d0e0f78ed6c8f7fa092ea924ce115d3
davidcrc <david_drcr@hotmail.com>
2018-02-20 16:40:38 -0500

    02.Componentes reutilizables (artistas)

--- App.js
+++ App.js
@@ -1,40 +1,31 @@
 import React, { Component } from 'react';
 import {  StyleSheet,  Text,  View,  Button,  Image} from 'react-native';
 import Icon from 'react-native-vector-icons/Ionicons';
+import ArtistBox from './ArtistBox'
 
 export default class AwesomeProject extends Component {
     
     render() {
-        const image = 'https://www.billboard.com/chapiv1/images/pref_images/q11170gagv8.jpg'
-        const name = 'Bruno Mars'
-        const likes = 200
-        const comments = 140
 
-    return (
-        <View style={styles.container}>
-            <View style={styles.artistBox}>
-                <Image style={styles.image} source={{uri:image }}  />
+        const artist = {        // se creo una variable con varios objetos
+            image: 'https://www.billboard.com/chapiv1/images/pref_images/q11170gagv8.jpg',
+            name: 'Bruno Mars',
+            likes: 200,
+            comments: 140,
+        }
 
-                <View style={styles.info}>
-                    <Text style={styles.name} > {name}</Text>
-                    <View style={styles.row} >
-                        <View style={styles.iconContainer} >
-                        
-                            <Icon name="ios-heart-outline" size={30} color="gray" />
-                            <Text style={styles.count} > {likes} </Text>
-                        </View>
-                        
-                        <View style={styles.iconContainer} >                        
-                            <Icon name="ios-chatboxes-outline" size={30} color="gray" />
-                            <Text style={styles.count}> {comments} </Text>
-                        </View>
-                        
-                    </View>
-                        
-                </View>
+        return (
+            <View style={styles.container}>
+                <Text style={styles.titulo} > DavisofT - Testing </Text>
+                
+                {/* La variable es pasada creando una property artist={} */}
+                <ArtistBox artist={artist} />       
+                <ArtistBox artist={artist} />       
+                <ArtistBox artist={artist} />       
+                <ArtistBox artist={artist} />       
+                
             </View>
-        </View>
-    );
+        );
   }
 }
 
@@ -49,39 +40,10 @@ const styles = StyleSheet.create({
         // flexWrap: 'wrap',                           // cae o voltea cuando no entra en toda la pantalla
         paddingTop: 3,
     },
-    artistBox: {
-        backgroundColor: 'white',
+    titulo: {
         flexDirection: 'row',
-    },
-
-    image: {
-        width: 150,
-        height: 150,
-    },
-    info: {
-        // backgroundColor: 'blue',
-        flex: 1, 
-        flexDirection: 'column',
         justifyContent: 'center',
-        alignItems: 'center',
-
-    },
-    name: {
-        marginTop: 10,
         color: '#333',
+       
     },
-    row: {
-        flexDirection: 'row',
-        // backgroundColor: 'red',
-        justifyContent: 'space-between',
-        marginHorizontal: 40,
-        marginTop: 15,
-    },
-    iconContainer: {
-        flex:1,
-        alignItems: 'center',
-    },
-    count: {
-        color: 'gray',
-    }
 });
