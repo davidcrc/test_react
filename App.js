82207c38a61958be41a8a89021015a67589c19b9
davidcrc <david_drcr@hotmail.com>
2018-02-20 11:53:23 -0500

    01.Armando la ArtistBox

--- App.js
+++ App.js
@@ -1,14 +1,38 @@
 import React, { Component } from 'react';
-import {  StyleSheet,  Text,  View,  Button,  Alert} from 'react-native';
+import {  StyleSheet,  Text,  View,  Button,  Image} from 'react-native';
+import Icon from 'react-native-vector-icons/Ionicons';
 
 export default class AwesomeProject extends Component {
     
     render() {
+        const image = 'https://www.billboard.com/chapiv1/images/pref_images/q11170gagv8.jpg'
+        const name = 'Bruno Mars'
+        const likes = 200
+        const comments = 140
+
     return (
         <View style={styles.container}>
-            <View style={[styles.box, styles.red]} />
-            <View style={[styles.box, styles.green]} />
-            <View style={[styles.box, styles.blue]} />
+            <View style={styles.artistBox}>
+                <Image style={styles.image} source={{uri:image }}  />
+
+                <View style={styles.info}>
+                    <Text style={styles.name} > {name}</Text>
+                    <View style={styles.row} >
+                        <View style={styles.iconContainer} >
+                        
+                            <Icon name="ios-heart-outline" size={30} color="gray" />
+                            <Text style={styles.count} > {likes} </Text>
+                        </View>
+                        
+                        <View style={styles.iconContainer} >                        
+                            <Icon name="ios-chatboxes-outline" size={30} color="gray" />
+                            <Text style={styles.count}> {comments} </Text>
+                        </View>
+                        
+                    </View>
+                        
+                </View>
+            </View>
         </View>
     );
   }
@@ -17,31 +41,47 @@ export default class AwesomeProject extends Component {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        flexDirection: 'row',
+        flexDirection: 'column',
         // justifyContent: 'center',            // sobre el eje primario , sea row o column 
         // justifyContent: 'space-around',         // espaciado entre el inicio y los objetos
         // alignItems: 'center',                   // sobre el eje secundario (contrario del primario)
-        backgroundColor: '#F5FCFF',
-        flexWrap: 'wrap',                           // cae o voltea cuando no entra en toda la pantalla
+        backgroundColor: 'lightgray',
+        // flexWrap: 'wrap',                           // cae o voltea cuando no entra en toda la pantalla
+        paddingTop: 3,
     },
-    box: {
-        width: 200,
-        height: 200,
-        backgroundColor: 'black'
-    },
-
-    red: {
-        backgroundColor: 'red',
-        // flex: 2,
-        // alignSelf: 'flex-end'  // alinea abajo o arriba
+    artistBox: {
+        backgroundColor: 'white',
+        flexDirection: 'row',
     },
 
-    green: {
-        // flex: 1,       
-        backgroundColor: 'green'
+    image: {
+        width: 150,
+        height: 150,
     },
+    info: {
+        // backgroundColor: 'blue',
+        flex: 1, 
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center',
 
-    blue: {
-        backgroundColor: 'blue'
     },
+    name: {
+        marginTop: 10,
+        color: '#333',
+    },
+    row: {
+        flexDirection: 'row',
+        // backgroundColor: 'red',
+        justifyContent: 'space-between',
+        marginHorizontal: 40,
+        marginTop: 15,
+    },
+    iconContainer: {
+        flex:1,
+        alignItems: 'center',
+    },
+    count: {
+        color: 'gray',
+    }
 });
